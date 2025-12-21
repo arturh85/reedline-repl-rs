36a3535dfaf239e2fb3b1dc3990bc99758d4ea6a
Artur Hallmann <arturh@arturh.de>
2025-12-21 17:30:19 +0100

    fix: linter issues and warnings

--- src/prompt.rs
+++ src/prompt.rs
@@ -9,26 +9,26 @@ pub struct ReplPrompt {
 
 impl Prompt for ReplPrompt {
     /// Use prefix as render prompt
-    fn render_prompt_left(&self) -> Cow<str> {
+    fn render_prompt_left(&self) -> Cow<'_, str> {
         {
             Cow::Borrowed(&self.prefix)
         }
     }
 
     // call default impl
-    fn render_prompt_right(&self) -> Cow<str> {
+    fn render_prompt_right(&self) -> Cow<'_, str> {
         self.default.render_prompt_right()
     }
-    fn render_prompt_indicator(&self, edit_mode: PromptEditMode) -> Cow<str> {
+    fn render_prompt_indicator(&self, edit_mode: PromptEditMode) -> Cow<'_, str> {
         self.default.render_prompt_indicator(edit_mode)
     }
-    fn render_prompt_multiline_indicator(&self) -> Cow<str> {
+    fn render_prompt_multiline_indicator(&self) -> Cow<'_, str> {
         self.default.render_prompt_multiline_indicator()
     }
     fn render_prompt_history_search_indicator(
         &self,
         history_search: PromptHistorySearch,
-    ) -> Cow<str> {
+    ) -> Cow<'_, str> {
         self.default
             .render_prompt_history_search_indicator(history_search)
     }
